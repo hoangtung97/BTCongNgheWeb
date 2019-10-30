d1c36cdf41f5c24051b4dc0b969528e7e620cbbc
MinhHieu <rezo.krifferson@gmail.com>
2019-10-30 09:36:41 +0700

    aa

--- WebChat/WebChat/Controllers/MainController.cs
+++ WebChat/WebChat/Controllers/MainController.cs
@@ -28,7 +28,7 @@ namespace WebChat.Controllers
             HttpCookie cookie = Request.Cookies["user"];
             UserCredential currentUser = new UserCredential(Int32.Parse(cookie.Value));
 
-            return View( Controllers.DbModuls.DbGet.getRoomList( currentUser.userID ));
+            return View( Controllers.DbModuls.DbGet.getUserRoomList( currentUser.userID ));
         }
     }
 }
