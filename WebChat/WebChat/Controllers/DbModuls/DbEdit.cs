1663af1d9e3755452711a8414104d64bfdc63e10
MinhHieu <rezo.krifferson@gmail.com>
2019-11-01 09:49:33 +0700

    aa

--- WebChat/WebChat/Controllers/DbModuls/DbEdit.cs
+++ WebChat/WebChat/Controllers/DbModuls/DbEdit.cs
@@ -12,18 +12,20 @@ namespace WebChat.Controllers.DbModuls
         public static Models.ChatWebsiteEntities database = new Models.ChatWebsiteEntities();
 
         //sua ten hien thi cua nguoi dung
-        public static void editUserDisplayName( int userID, string newDisplayName )
+        public static void editUser( Models.User newUser )
         {
             Models.User user = new Models.User();
 
-            user = database.Users.Single(u => u.UserID == userID);
-            user.DisplayName = newDisplayName;
+            user = database.Users.Single(u => u.UserID == newUser.UserID);
+            user.DisplayName = newUser.DisplayName;
 
+            //o day chua co luu anh 
+            //do dang nghien cuu cach luu tru anh trong csdl
             database.SaveChanges();
         }
 
         //sua ten phong
-        public static void editRoomName( int roomID, string newRoomName)
+        public static void editRoom( int roomID, string newRoomName)
         {
             Models.ChatRoom room = new Models.ChatRoom();
 
