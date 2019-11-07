231ff61787e2febfd675cff3627985db9609641e
tamerlane2998 <trieuquangminh29@gmail.com>
2019-11-07 08:55:54 +0700

    up lai file js

---
+++ WebChat/WebChat/Content/js/message.js
@@ -0,0 +1,21 @@
+﻿function ShowTypedMessage() {
+    var message = document.getElementById("InputMessage").value;
+    if (message != "") {
+        var addli = document.createElement("li");
+        var adddiv = document.createElement("div");
+        adddiv.classList.add("Out");
+        adddiv.innerHTML = '<div class="contentOut"><div class="message"><div class="bubbleOut"><p class="pOut">' + message + '</p></div></div></div>';
+        addli.appendChild(adddiv);
+        document.getElementById("message-container").appendChild(addli);
+        document.getElementById("InputMessage").value = "";
+    }   
+}
+
+function ShowInboxMessage(message, name) {
+    var addli = document.createElement("li");
+    var adddiv = document.createElement("div");
+    adddiv.classList.add("In");
+    adddiv.innerHTML = '<span>' + name + '</span><div class="contentIn"><div class="message"><div class="bubbleIn"><p class ="pIn">' + message + '</p></div></div></div>';
+    addli.appendChild(adddiv);
+    document.getElementById("message-container").appendChild(addli);
+}
